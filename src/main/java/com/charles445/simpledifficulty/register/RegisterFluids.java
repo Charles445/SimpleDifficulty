0d8f257d4631f8fab35b3b1a5f23658787b9f214
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-01-23 19:42:59 +0100

    Add purified water biome color handler

--- src/main/java/com/charles445/simpledifficulty/register/RegisterFluids.java
+++ src/main/java/com/charles445/simpledifficulty/register/RegisterFluids.java
@@ -8,11 +8,20 @@ import com.charles445.simpledifficulty.fluid.FluidBasic;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.world.biome.BiomeColorHelper;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class RegisterFluids
@@ -30,10 +39,11 @@ public class RegisterFluids
 			
 			//Create Fluids
 			purifiedWater = new FluidBasic(
-					"purifiedwater",
-					"purifiedwater_still",
-					"purifiedwater_flow",
-					0xFF44AFF5);
+				"purifiedwater",
+				"purifiedwater_still",
+				"purifiedwater_flow",
+				0xFF44AFF5
+			);
 			
 			//Fluids register themselves
 			
@@ -46,7 +56,7 @@ public class RegisterFluids
 				registry.register(fluidBlocks.get(key));
 			}
 		}
-		
+
 		@SubscribeEvent
 		public static void registerFluidItems(RegistryEvent.Register<Item> event)
 		{
@@ -64,6 +74,39 @@ public class RegisterFluids
 			itemblock.setRegistryName(block.getRegistryName());
 			itemblock.setCreativeTab(ModCreativeTab.instance);
 			return itemblock;
+		}
+
+		@SideOnly(Side.CLIENT)
+		@SubscribeEvent
+		public static void registerBlockColourHandlers(final ColorHandlerEvent.Block event) {
+			final BlockColors blockColors = event.getBlockColors();
+
+			// Use the water colour of the biome or the default water colour
+			final IBlockColor waterColourHandler = (state, blockAccess, pos, tintIndex) -> {
+				if (blockAccess != null && pos != null) {
+					return BiomeColorHelper.getWaterColorAtPos(blockAccess, pos);
+				}
+
+				return 0xFF44AFF5;
+			};
+
+			blockColors.registerBlockColorHandler(waterColourHandler, blockPurifiedWater);
+		}
+
+		@SideOnly(Side.CLIENT)
+		@SubscribeEvent
+		public static void registerItemColourHandlers(final ColorHandlerEvent.Item event) {
+			final BlockColors blockColors = event.getBlockColors();
+			final ItemColors itemColors = event.getItemColors();
+
+			// Use the Block's colour handler for an ItemBlock
+			final IItemColor itemBlockColourHandler = (stack, tintIndex) -> {
+				@SuppressWarnings("deprecation")
+				final IBlockState state = ((ItemBlock) stack.getItem()).getBlock().getStateFromMeta(stack.getMetadata());
+				return blockColors.colorMultiplier(state, null, null, tintIndex);
+			};
+
+			itemColors.registerItemColorHandler(itemBlockColourHandler, blockPurifiedWater);
 		}
 	}
 }
